429567858ae4deb0c2a39abb75d2107bbfe42445
Kameron Shahabi <kyshahab@usc.edu>
2020-11-05 17:05:55 -0800

    server update

--- src/ServerMain.java
+++ src/ServerMain.java
@@ -1,8 +1,40 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class ServerMain {
 	
+	private ArrayList<ServerThread> serverThreads;
+	
+	public ServerMain(int port)
+	{
+		try
+		{
+			System.out.println("Binding to port " + port);
+			ServerSocket ss = new ServerSocket(port);
+			serverThreads = new ArrayList<>();
+			while(true)
+			{
+				Socket s = ss.accept();   //  Accept the incoming request
+				System.out.println("Connection from " + s + " at " + new Date());
+				ServerThread st = new ServerThread(s, this);
+				System.out.println("Adding this client to active client list");
+				serverThreads.add(st);
+			}
+		}
+		catch (Exception ex) {}
+
+	}
+	
+	// send new list of scores to every client
+	public void sendScores(ArrayList<Score> scores)
+	{
+		
+	}
+
 	public static void main(String [] args)
 	{
-		System.out.println(Database.egg());
+		new ServerMain(6789);
 	}
 }
--- src/ServerThread.java
+++ src/ServerThread.java
@@ -1,4 +1,78 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
 
-public class ServerThread {
+public class ServerThread extends Thread {
+
+	private PrintWriter pw;
+	private BufferedReader br;
+	private ServerMain sm;
+	private ArrayList<Score> highScores;
+
+	public ServerThread(Socket s, ServerMain sm)
+	{
+		//your code here
+		try {
+			this.sm = sm;
+	
+			br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			pw = new PrintWriter(s.getOutputStream(), true);
+			
+			// send this originally
+			highScores = Database.getHighScores();
+			
+			this.start();
+		}
+		
+		catch(Exception e) {}
+	}
+	
+	// sends score to client
+	public void sendScore()
+	{
+		
+	}
+	
+	public synchronized boolean notEqual(ArrayList<Score> check)
+	{
+		if (highScores.size() != check.size())
+		{
+			return true;
+		}
+
+		for (int i = 0; i < check.size(); i++)
+		{
+			String name1 = check.get(i).getName();
+			String name2 = highScores.get(i).getName();
+			int score1 = check.get(i).getScore();
+			int score2 = highScores.get(i).getScore();
+			
+			if (!name1.equals(name2) || score1 != score2)
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void run()
+	{
+		//your code here
+		ArrayList<Score> temp = null;
+		while (true)
+		{
+			try
+			{
+				if (notEqual(temp))
+				{
+					sm.sendScores(temp);
+					highScores = temp;
+				}
+			}
+			catch (Exception e) {}
+		}
+	}
 
 }
